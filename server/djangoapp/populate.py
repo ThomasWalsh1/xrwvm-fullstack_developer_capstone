8e58aa551a445994a57d9ee61a9959f07acf0615
ThomasWalsh1 <130932404+ThomasWalsh1@users.noreply.github.com>
2024-10-17 22:38:50 -0400

    Update populate.py lint fix 3

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -45,9 +45,8 @@ def initiate():
           "car_make": car_make_instances[4]},
       {"name": "Camry", "type": "Sedan", "year": 2023,
           "car_make": car_make_instances[4]},
-      {"name": "Kluger", "type": "SUV", "year": 2023, 
+      {"name": "Kluger", "type": "SUV", "year": 2023,
           "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
